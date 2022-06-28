e081f70e48fed1fcac25eef151104958cebf8a7c
kushalbadal <50290182+kushalbadal@users.noreply.github.com>
2022-06-28 16:03:41 +0545

    Update validate.py

--- validate.py
+++ validate.py
@@ -3,15 +3,27 @@ def validate(dict,int_type,str_type):
     result="True"
     for k in dict:
         if type(dict[k]) == int:
+          if k!="name" and k!="Married":
             if dict[k]>int_type["max"] or dict[k]<int_type["min"]:
                 result="False"
                 break
+          else:
+            result="False"
+            break
         if type(dict[k])==str:
+          if k!="age" and k!="Married":
             if len(dict[k])>str_type["max"] or len(dict[k])<str_type["min"]:
                  result="False"
                  break
+          else:
+            result="False"
+            break
         if type(dict[k])==bool:
+          if k!="age" and k!="name":
             pass
+          else:
+            result="False"
+            break
     return result
 
 
