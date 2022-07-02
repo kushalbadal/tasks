28138b16c4eb0d2f99f24013c6e203b17446cb38
kushalbadal <50290182+kushalbadal@users.noreply.github.com>
2022-07-02 14:27:28 +0545

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,21 +1,38 @@
 # validation function
 def validate(dict,validate_rule):
   k="All"
-  result="True"
+  result=True
   for key, value in dict.items():
-    if (validate_rule.get(key, lambda x: False)(value))== False:
-      result="False"
-      k=key
-      break
+    for i in value["value"]:
+      if (validate_rule.get(key, lambda x: False)(i))== False:
+        result=False
+        k=key
+        break
   return k,result
 
 
-dict={"name":"kushal","age":25,"married":False}
-validate_rule = {
-    "name": lambda x: isinstance(x, str) and 4 <=len(x)<=10,
-    "age": lambda x: isinstance(x, int) and 16 <=x <=80,
-    "married": lambda x: isinstance(x, bool) ,
-}
+dict={"name":{"value":["kushal","Nabin"],"data_type":str},
+      "age":{"value":[25,28,34],"data_type":int},
+      "married":{"value":[True],"data_type":bool},
+      "address":{"value":["bhaktapur"],"data_type":str}
+      }
+string_rule={"min_length":4,
+                     "max_length":10}
+integer_rule= {"min_value":16,
+                      "max_value":80}
+
+validate_rule={}
+for key,v in dict.items():
+  if v["data_type"]==str:
+    validate_rule[key]= lambda x: isinstance(x,str) and string_rule["min_length"] <=len(x)<=string_rule["max_length"]
+  
+  if v["data_type"]==int:
+    validate_rule[key]= lambda x: isinstance(x,int) and integer_rule["min_value"] <= x <=integer_rule["max_value"]
+  
+
+  if v["data_type"]==bool:
+    validate_rule[key]= lambda x: isinstance(x,bool)
+
 
 k,result=validate(dict,validate_rule)
 print(f"{k} Field is {result}")
